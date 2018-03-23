a1969b1eb56b3db79181353460fb6169adadc35c
Hamed.Sharif <hamed_shereef@yahoo.com>
2018-03-23 12:24:06 +0100

    list the products

--- src/g/app.js
+++ src/g/app.js
@@ -1,1 +1,56 @@
 // JavaScript för att implementera kraven A-E.
+
+
+
+    /*//push to Json database
+    let temp = new Object();
+    temp["id"] = "po-0167";
+    temp["date"] = new Date();
+    temp["quantity"] = 1;
+    temp["amount"] = 4;
+    temp["title"] = "A Book About Nothing";
+
+    bookorders.push(temp);
+*/
+
+
+
+
+let output = $('#output');
+let request = new XMLHttpRequest();
+request.open("GET", "http://demo.edument.se/api/products", "http://demo.edument.se/api/content");
+
+// XMLHRequest
+$("#xhr").click(()=>{
+
+
+
+    request.onreadystatechange = ()=>{
+    if (request.readyState === XMLHttpRequest.DONE){
+        if (request.status === 200){
+            /*let p = $("<p></p>").text(JSON.parse(request.responseText));
+            output.append(p);*/
+            let xData =JSON.parse(request.responseText)
+            addHtml(xData);
+        }else {
+            document.write('error');
+        }
+    }
+};
+request.send();
+});
+
+
+function addHtml(data) {
+
+    let htmlText ="";
+
+    for (i=0;i<data.length;i++){
+
+        htmlText += "<br><br>" + "ID Number " + data[i].Id+ " Have name : " + " "+  data[i].Name + "<br> <br>" + data[i].Image ;
+
+    }
+
+    output.innerHTML=document.write(htmlText);
+}
+
